b718cb9705bd8ec3e4692f825daa1b033a43f3d5
AbubakirDev-react <2000ak47123456@gmail.com>
2026-06-12 18:45:58 +0500

    version 1.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,7 @@ const Navbar = ({}) => {
             <li className='nav-item'><Link to="/" className='nav-link'><i className='bi bi-house-fill'></i><span> Home</span></Link></li>
             <li className='nav-item'><Link to="/favourites/" className='nav-link'><i className='bi bi-heart'></i> <span> Favourites</span></Link></li>
             <li className='nav-item'><Link to="/checkout/" className='nav-link'><i className='bi bi-cart'></i> <span> Cart</span></Link></li>
-            <li className='nav-item'><a href="#" className='nav-link'><i className='bi bi-box'></i> <span> Deliveries</span></a></li>
-            <button className='btn text-primary fs-4'><i className="bi bi-moon-stars"></i></button>
-        </ul>
+            </ul>
         </div>
       </nav>
   )

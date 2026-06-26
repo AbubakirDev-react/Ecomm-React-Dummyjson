9c1aa61d698e126ae9313c68b096390d97d7beaf
AbubakirDev-react <2000ak47123456@gmail.com>
2026-06-26 20:53:55 +0500

    created vite project with bun, added :root to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Products from './data/products'
-import { Routes,Route } from 'react-router-dom'
-import ProductDetail from './pages/ProductDetail'
-import Favourites from './pages/Favourites'
-import FavouriteProvider from './context/favouriteContext'
-import CartProvider from './context/cartContext'
-import Checkout from './pages/checkout'
 
 function App() {
-  const [ favourites,setFavourites ] = useState([]);
+  const [count, setCount] = useState(0)
+
   return (
     <>
-    <CartProvider>
-      <FavouriteProvider>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home setFavourites={setFavourites} />} />
-          <Route path='/product/:id' element={<ProductDetail />} />
-          <Route path='/favourites/' element={<Favourites favourites={favourites} /> } />
-          <Route path='/checkout/' element={<Checkout />} />
-        </Routes>
-        <Footer/>
-      </FavouriteProvider>
-    </CartProvider>
+      
     </>
   )
 }
